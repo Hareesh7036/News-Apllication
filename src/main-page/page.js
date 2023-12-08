78180898fe4cd7a8ff5ca3d21fb84d3873324418
hareesh <madahareesh12@gmail.com>
2023-12-08 13:22:40 +0530

    content container built

--- src/main-page/page.js
+++ src/main-page/page.js
@@ -4,6 +4,7 @@ import './main.css'
 import Reverse from 'my-sample-react-lib-ntg'
 
 const Content = () => {
+    const[clickedDataId,setClickedDataId]=useState(0);
     const[newsData,setNewsData]=useState();
     useEffect(()=>{
         fetch('https://newsapi.org/v2/everything?q=tesla&from=2023-11-08&sortBy=publishedAt&apiKey=4cf547b050224c05aac85694217fe43b')
@@ -20,6 +21,10 @@ const Content = () => {
     })
     },[]);
 
+    const handleContent= (eve)=>{
+        setClickedDataId(eve?.target.closest('li').id);
+    };
+
   return (
     <div className='row main-container'>
         
@@ -28,16 +33,30 @@ const Content = () => {
         <p>Total results:{newsData?.totalResults}</p>
         <ul className='content-list'>
             {newsData?.articles.map((article,ind)=>{
-             return(   <li key={`li_${ind}`}>
-                    <div>Content:{article.content}</div>
-                    <div>Description:{article.description}</div>
+             return(   <li key={`li_${ind}`} onClick={handleContent} id={ind}>
+                    <div>
+                        <img src={article.urlToImage} className='headline-img'></img>
+                    </div>
+                    <div>Content:{article.title}</div>
+                    
+                    {/* <div>Description:{article.description}</div> */}
                     <div>Published At:{article.publishedAt}</div>
                 </li>)
             })}
         </ul>
         </div>
-        <div className='col-sm-7'>
-            <p>hello guys</p>
+        <div className='col-sm-7 content-container'>
+            <div >
+                <p>{newsData?.articles[clickedDataId].title}</p>
+                <img className='content-img' src={newsData?.articles[clickedDataId].urlToImage}></img>
+                <div>
+                    <p>{newsData?.articles[clickedDataId].description}</p>
+                    <p>{newsData?.articles[clickedDataId].content}</p>
+                    </div>
+                <p>- {newsData?.articles[clickedDataId].author}</p>
+                <a href={newsData?.articles[clickedDataId].url}>click for more detailss</a>
+            </div>
+            
         </div>
     </div>
   )
