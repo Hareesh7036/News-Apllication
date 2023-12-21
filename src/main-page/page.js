32bbf5fa870fe140dce8dd7597e4ea82a05ec9e8
hareesh <madahareesh12@gmail.com>
2023-12-21 14:54:21 +0530

    updated some css and did testing

--- src/main-page/page.js
+++ src/main-page/page.js
@@ -1,9 +1,17 @@
 'use client';
 import React,{useEffect,useState} from 'react'
 import './main.css'
-import Reverse from 'my-sample-react-lib-ntg'
+
+const Loader=()=>{
+    return(<>
+    
+    <img src="https://i.gifer.com/ZZ5H.gif" alt="Loading...." className="loader" />
+    </>);
+    };
 
 const Content = () => {
+    const [showLoader,setShowLoader]=useState(false);
+    const [category,setCategory]=useState('apple');
     const[clickedDataId,setClickedDataId]=useState(0);
     const[newsData,setNewsData]=useState();
     const todayDate= new Date();
@@ -11,19 +19,20 @@ const Content = () => {
     const month=todayDate.getMonth();
     const year =todayDate.getFullYear();
     useEffect(()=>{
-        fetch(`https://newsapi.org/v2/everything?q=tesla&from=${year}-${month}-${date}&language=en&sortBy=publishedAt&apiKey=4cf547b050224c05aac85694217fe43b`)
+        setShowLoader(true);
+        fetch(`https://newsapi.org/v2/everything?q=${category}&from=${year}-${month}-${date}&language=en&sortBy=publishedAt&apiKey=4cf547b050224c05aac85694217fe43b`)
     .then((res)=>{
         const x=res.json();
         return x;
     })
     .then((res)=>{
-        console.log(res);
         setNewsData(res);
+        setShowLoader(false);
     })
     .catch((res)=>{
         console.log(res);
     })
-    },[]);
+    },[category]);
 
     const handleContent= (eve)=>{
         setClickedDataId(eve?.target.closest('li').id);
@@ -37,13 +46,24 @@ const Content = () => {
     const handleContentBtn =()=>{
         setHeadlines(false);
     };
+
+    const handleApple = ()=>{
+        setCategory("apple");
+    };
+
+    const handleTesla=()=>{
+        console.log("oyy")
+        setCategory("tesla")
+    }
+
   return (
+    
     <div className='row main-container'>
-        
-        <div className="headlines col-sm-5 " style={{zIndex:headlines?"0":"2"}}>
-        <h3>Headlines</h3>
+        {showLoader && <Loader/>}
+        <div className="headlines col-sm-5 " style={{zIndex:headlines?"0":"2"}} data-testid='headlines-cont'>
+        <h3 data-testid="heading">Headlines</h3>
         <button className='headlines-btn ' onClick={handleClose}><span className='bi bi-x-lg'></span></button>
-        <p style={{fontSize:"13px"}}>Total results:{newsData?.totalResults}</p>
+        <p style={{fontSize:"13px"}} data-testid="results">Total results:{newsData?.totalResults}, From {category}</p>
         <ul className='content-list'>
             {newsData?.articles.map((article,ind)=>{
              return(   <li key={`li_${ind}`} onClick={handleContent} id={ind}>
@@ -60,7 +80,12 @@ const Content = () => {
         </div>
         <div className='col-sm-7 content-container' id='content-cont' >
             <div >
+
+                <div className='content-btn-container'>
+                    <button className='news' onClick={handleApple}>Apple</button>
+                    <button className='news' onClick={handleTesla}>Tesla</button>
                 <button className='content-btn' onClick={handleContentBtn}>Headlines</button>
+                </div>
                 <p className='title'>{newsData?.articles[clickedDataId].title}</p>
                 <img className='content-img' src={newsData?.articles[clickedDataId].urlToImage}></img>
                 <div>
